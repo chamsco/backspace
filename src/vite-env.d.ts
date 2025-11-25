3c1946750f855502d9215d3931f542c40045e527
Ebagnitchie Charles-Emmanuel <xo@MacBook-Pro-de-Ebagnitchie.local>
2025-11-25 22:07:41 +0100

    Fix TypeScript SVG import error for deployment
    - Add vite-env.d.ts with SVG module declarations
    - Fixes build error: Cannot find module '../media/backspace logo.svg'

---
+++ src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg?react' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
